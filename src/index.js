3acb6eca905e636e760f772e0037a00fb5bba4a2
Ice-Citron <shng2025@gmail.com>
2024-08-11 17:05:38 +0800

    tutorial of firebase init finished

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC6zfUmk7uw8W9BRNt8lByJNFx9w5TsGz4",
-  authDomain: "getting-started-with-fir-bebcb.firebaseapp.com",
-  projectId: "getting-started-with-fir-bebcb",
-  storageBucket: "getting-started-with-fir-bebcb.appspot.com",
-  messagingSenderId: "15915151713",
-  appId: "1:15915151713:web:dbba509a6b7eee961e1520",
-  measurementId: "G-L9DRCP865K"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyC6zfUmk7uw8W9BRNt8lByJNFx9w5TsGz4",
+    authDomain: "getting-started-with-fir-bebcb.firebaseapp.com",
+    projectId: "getting-started-with-fir-bebcb",
+    storageBucket: "getting-started-with-fir-bebcb.appspot.com",
+    messagingSenderId: "15915151713",
+    appId: "1:15915151713:web:dbba509a6b7eee961e1520",
+    measurementId: "G-L9DRCP865K"
+  });
+
+const auth = getAuth(firebaseApp);
+
+
+// Detect auth state
+onAuthStateChanged(auth, (user) => {
+    if (user != null) {
+        console.log('User is signed in');
+    } 
+    else {
+        console.log('User is signed out');
+    }
+})
+
+
+
+// npm i webpack webpack-cli -D 
+// node_modules/.bin/webpack --mode=development --entry ./src/index.js -o dist
+
